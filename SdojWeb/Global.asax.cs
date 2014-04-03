a99253bf415d47f1702f25af2ee5d174109bc092
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-04-03 21:18:11 +0800

    small change

--- SdojWeb/Global.asax.cs
+++ SdojWeb/Global.asax.cs
@@ -60,7 +60,7 @@ namespace SdojWeb
                 foreach (var task in container.GetAllInstances<IRunAtStartup>())
                 {
                     task.Execute();
-                }
+                } 
             }
         }
 
