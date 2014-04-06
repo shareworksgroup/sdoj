a6afc50977f95cceca940b9cb2d7337d872fd113
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-04-06 13:13:48 +0800

    推进推进。

---
+++ SdojWeb/Controllers/SolutionController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Microsoft.AspNet.Identity;
+using SdojWeb.Models;
+
+namespace SdojWeb.Controllers
+{
+    public class SolutionController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public SolutionController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: Solution
+        public async Task<ActionResult> Index()
+        {
+            return View(await _dbContext.Solutions.ToListAsync());
+        }
+
+        // GET: Solution/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Solution solution = await _dbContext.Solutions.FindAsync(id);
+            if (solution == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solution);
+        }
+
+        // GET: Solution/Create/id
+        public ActionResult Create(int? id)
+        {
+            var solutionCreateModel = new SolutionCreateModel {QuestionId = id??0};
+            return View(solutionCreateModel);
+        }
+
+        // POST: Solution/Create/id
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(
+            [Bind(Include = "QuestionId,Language,Source")] SolutionCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var question = Mapper.Map<Solution>(model);
+                
+                _dbContext.Solutions.Add(question);
+                try
+                {
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        // GET: Solution/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Solution solution = await _dbContext.Solutions.FindAsync(id);
+            if (solution == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solution);
+        }
+
+        // POST: Solution/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(
+            [Bind(Include = "Id,ApplicationUserId,Language,Source,Status,SubmitTime")] Solution solution)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(solution).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(solution);
+        }
+
+        // GET: Solution/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Solution solution = await _dbContext.Solutions.FindAsync(id);
+            if (solution == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solution);
+        }
+
+        // POST: Solution/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Solution solution = await _dbContext.Solutions.FindAsync(id);
+            _dbContext.Solutions.Remove(solution);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- SdojWeb/Models/SolutionModels.cs
+++ SdojWeb/Models/SolutionModels.cs
@@ -5,25 +5,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using Microsoft.AspNet.Identity;
+using SdojWeb.Infrastructure.Mapping;
 
 namespace SdojWeb.Models
 {
     public class Solution
     {
-        [HiddenInput]
         public int Id { get; set; }
 
-        [HiddenInput, Required]
-        public ApplicationUser CreateUser { get; set; }
+        public virtual ApplicationUser CreateUser { get; set; }
 
-        [HiddenInput, Required]
-        public Question Question { get; set; }
+        public string CreateUserId { get; set; }
 
-        [Display(Name = "语言")]
+        public virtual Question Question { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
         public Languages Language { get; set; }
 
         [Display(Name = "代码"), Required]
-        public string Code { get; set; }
+        public string Source { get; set; }
 
         [Display(Name = "状态")]
         public SolutionStatus Status { get; set; }
@@ -32,8 +36,30 @@ namespace SdojWeb.Models
         public DateTime SubmitTime { get; set; }
     }
 
+    public class SolutionCreateModel : IHaveCustomMapping
+    {
+        [Display(Name = "题目"), Required]
+        public int QuestionId { get; set; }
+
+        [Display(Name = "语言"), Required]
+        public Languages Language { get; set; }
+
+        [Display(Name = "源代码"), Required, DataType(DataType.MultilineText)]
+        public string Source { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<SolutionCreateModel, Solution>()
+                .ForMember(dest => dest.SubmitTime, source => source.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.CreateUserId, source => source.MapFrom(x => HttpContext.Current.User.Identity.GetUserId()))
+                .ForMember(dest => dest.Status, source => source.MapFrom(x => SolutionStatus.Queuing))
+                .ForMember(dest => dest.QuestionId, source => source.MapFrom(x => x.QuestionId));
+        }
+    }
+
     public enum SolutionStatus
     {
+        [Display(Name = "排队中")]
         Queuing, 
         Juding, 
         Accepted, 
@@ -42,6 +68,11 @@ namespace SdojWeb.Models
 
     public enum Languages
     {
+        [Display(Name = "C#")]
         CSharp,
+        [Display(Name = "Visual Basic")]
+        VB, 
+        [Display(Name = "C++")]
+        Cpp, 
     }
 }
