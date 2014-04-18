2e8a71da12e99847b5249f66f0c48fbe0d349c4f
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-04-18 08:59:15 +0800

    给配置文件加入username和password。

--- SdojClient/app_config.cpp
+++ SdojClient/app_config.cpp
@@ -8,6 +8,11 @@ void app_config::load_from_file(string filename)
 {
 	wptree pt;
 	read_xml(filename, pt);
-	server = pt.get<wstring>(L"config.server");
-	port = pt.get<short>(L"config.port");
+
+	auto config = pt.get_child(L"A-Pa5sword-That:Never8eenUsed");
+
+	server = config.get<wstring>(L"server");
+	port = config.get<short>(L"port");
+	username = config.get<wstring>(L"username");
+	password = config.get<wstring>(L"password");
 }
--- SdojClient/app_config.h
+++ SdojClient/app_config.h
@@ -4,5 +4,8 @@ struct app_config
 {
 	wstring server;
 	short port;
+	wstring username;
+	wstring password;
+
 	void load_from_file(string filename);
 };
