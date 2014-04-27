b644d90f803711be62d0158e1227a13ffd96f85c
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-04-27 17:08:52 +0800

    加入角色管理。

--- SdojWeb/Controllers/IdentityController.cs
+++ SdojWeb/Controllers/IdentityController.cs
@@ -28,12 +28,14 @@ namespace SdojWeb.Controllers
             return RedirectToAction("Users");
         }
 
+        // GET: Identity/Users/
         public ActionResult Users()
         {
             var users = UserMgr.Users.Project().To<UserSummaryViewModel>().ToArray();
             return View(users);
         }
 
+        // GET: Identity/Edit/3
         public ActionResult Edit(int id)
         {
             var user = UserMgr.Users.Where(u => u.Id == id)
@@ -49,6 +51,7 @@ namespace SdojWeb.Controllers
             return View(roles);
         }
 
+        // POST: Identity/DeleteUserRole?userId=3&roleId=4
         [HttpPost, ValidateAntiForgeryToken]
         public ActionResult DeleteUserRole(int userid, int roleid)
         {
@@ -68,6 +71,7 @@ namespace SdojWeb.Controllers
             return action.WithSuccess("角色{0}删除成功。", role.Name);
         }
 
+        // POST: Identity/AddUserRole?userId=3&roleId=4
         [HttpPost, ValidateAntiForgeryToken]
         public ActionResult AddUserRole(int userid, int roleid)
         {
@@ -85,6 +89,51 @@ namespace SdojWeb.Controllers
             }
 
             return action.WithSuccess("角色{0}添加成功。", role.Name);
+        }
+
+        // GET: Identity/Roles
+        public ActionResult Roles()
+        {
+            var roles = RoleMgr.Roles.ToArray();
+            return View(roles);
+        }
+
+        // POST: Identity/AddRole?name=xyz
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult AddRole(string name)
+        {
+            var action = RedirectToAction("Roles");
+            if (RoleMgr.RoleExists(name))
+            {
+                return action.WithError("已经存在名为{0}的角色。", name);
+            }
+
+            var role = new ApplicationRole(name);
+            var result = RoleMgr.Create(role);
+            if (!result.Succeeded)
+            {
+                return action.WithError("新建角色失败，因为{0}", string.Join(",", result.Errors));
+            }
+            return action.WithSuccess("角色{0}新建成功。", role.Name);
+        }
+
+        // POST: Identity/DeleteRole?roleId=3
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult DeleteRole(int roleId)
+        {
+            var action = RedirectToAction("Roles");
+            var role = RoleMgr.FindById(roleId);
+            if (role == null)
+            { 
+                return action.WithError("不存在id为{0}的角色。", roleId);
+            }
+
+            var result = RoleMgr.Delete(role);
+            if (!result.Succeeded)
+            {
+                return action.WithError("删除角色失败，因为{0}", string.Join(",", result.Errors));
+            }
+            return action.WithSuccess("角色{0}删除成功。", role.Name);
         }
     }
 }
