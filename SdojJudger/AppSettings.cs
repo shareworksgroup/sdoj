38551c6b84cd0cafcb69c43d439627af28c2867f
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-17 22:32:03 +0800

    加入AppSettings。

---
+++ SdojJudger/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace SdojJudger
+{
+    public static class AppSettings
+    {
+        public static string ServerUrl
+        {
+            get { return ConfigurationManager.AppSettings["serverUrl"]; }
+        }
+
+        public static string UserName
+        {
+            get { return ConfigurationManager.AppSettings["username"]; }
+        }
+
+        public static string Password
+        {
+            get { return ConfigurationManager.AppSettings["password"]; }
+        }
+    }
+}
