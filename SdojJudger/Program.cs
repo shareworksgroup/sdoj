d5e520489757414378f18eaa39411413ed8585ae
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-18 21:44:47 +0800

    优化登录代码。

--- SdojJudger/Program.cs
+++ SdojJudger/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client;
 
 namespace SdojJudger
@@ -9,60 +11,53 @@ namespace SdojJudger
         static void Main(string[] args)
         {
             var connection = new HubConnection(AppSettings.ServerUrl);
-            Cookie returnedCookie;
 
-            var authResult = AuthenticateUser(AppSettings.UserName, AppSettings.Password, out returnedCookie);
+            var authCookie = AuthenticateUser(AppSettings.UserName, AppSettings.Password).Result;
 
-            if (authResult)
+            if (authCookie != null)
             {
                 connection.CookieContainer = new CookieContainer();
-                connection.CookieContainer.Add(returnedCookie);
+                connection.CookieContainer.Add(authCookie);
                 Console.WriteLine("Welcome " + AppSettings.UserName);
+
+                var hub = connection.CreateHubProxy(AppSettings.HubName);
+                hub.On("DoWork", str => Console.WriteLine(str));
+
+                connection.Start().Wait();
+
+                while (true)
+                {
+                    var line = Console.ReadLine();
+                    hub.Invoke("Send", line);
+                }
             }
             else
             {
                 Console.WriteLine("Login failed");
             }
-
-            var hub = connection.CreateHubProxy(AppSettings.HubName);
-            hub.On("DoWork", str => Console.WriteLine(str));
-
-            connection.Start().Wait();
-
-            while (true)
-            {
-                var line = Console.ReadLine();
-                hub.Invoke("Send", line);
-            }
         }
 
-        private static bool AuthenticateUser(string user, string password, out Cookie authCookie)
+        private static async Task<Cookie> AuthenticateUser(string user, string password)
         {
-            var request = WebRequest.Create(AppSettings.LoginUrl) as HttpWebRequest;
+            var request = WebRequest.CreateHttp(AppSettings.LoginUrl);
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = new CookieContainer();
 
-            var authCredentials = "username=" + user + "&password=" + password;
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(authCredentials);
+            var authCredentials = string.Format("username={0}&password={1}",
+                WebUtility.UrlEncode(user),
+                WebUtility.UrlEncode(password));
+            var bytes = Encoding.UTF8.GetBytes(authCredentials);
             request.ContentLength = bytes.Length;
-            using (var requestStream = request.GetRequestStream())
+
+            using (var requestStream = await request.GetRequestStreamAsync())
             {
                 requestStream.Write(bytes, 0, bytes.Length);
             }
 
-            using (var response = request.GetResponse() as HttpWebResponse)
+            using (var response = await request.GetResponseAsync() as HttpWebResponse)
             {
-                authCookie = response.Cookies[AppSettings.CookieName];
-            }
-
-            if (authCookie != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
+                return response.Cookies[AppSettings.CookieName];
             }
         }
     }
