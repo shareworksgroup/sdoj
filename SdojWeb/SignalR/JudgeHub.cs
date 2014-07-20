37b5b3dad46228e28c954ad4b93e1eca59cfb1d3
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-20 11:40:36 +0800

    完善Update和Lock（把Accpet方法改名为Lock）。

--- SdojWeb/SignalR/JudgeHub.cs
+++ SdojWeb/SignalR/JudgeHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
@@ -11,30 +12,70 @@ namespace SdojWeb.SignalR
     [Microsoft.AspNet.SignalR.Authorize(Roles = SystemRoles.Judger)]
     public class JudgeHub : Hub
     {
-        public void Send(string text)
-        {
-            var user = Context.User;
-            Clients.All.DoWork(text);
-        }
-
-        public void Update(int solutionId, 
-            SolutionStatus statusId, long runTimeTicks, long usingMemory, 
-            DateTime accessTime)
+        public async Task<bool> Update(int solutionId,
+            SolutionStatus statusId, int? runTimeMs, float? usingMemoryMb)
         {
             var db = DbContext;
-            var solution = db.Solutions.Find(solutionId);
-            if (solution != null)
+            var solutionLock = await db.SolutionLocks.FindAsync(solutionId);
+
+            // 未被锁住，或者锁住的客户端不正确，或者锁已经过期，则不允许操作。
+            if (solutionLock == null)
             {
-                
+                return false;
             }
+            if (solutionLock.LockClientId != Context.ConnectionId || solutionLock.LockEndTime > DateTime.Now)
+            {
+                db.Entry(solutionLock).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return false;
+            }
+
+            // 锁住，允许操作，然后改变状态。
+            var solution = await db.Solutions.FindAsync(solutionId);
+            solution.Status = statusId;
+            if (runTimeMs != null) solution.RunTime = runTimeMs.Value;
+            if (usingMemoryMb != null) solution.UsingMemoryMb = usingMemoryMb.Value;
+            solution.Lock = null;
+
+            // 删除锁，保存数据。
+            db.Entry(solution).State = EntityState.Modified;
+            db.Entry(solutionLock).State = EntityState.Deleted;
+            await db.SaveChangesAsync();
+
+            return true;
         }
 
-        public void Accept(int solutionId)
+        public async Task<bool> Lock(int solutionId)
         {
             var db = DbContext;
-            var solution = db.Solutions.Find(solutionId);
 
-            
+            var solutionLock = await db.SolutionLocks.FindAsync(solutionId);
+
+            // 已有锁并已被别人占用，直接失败。
+            if (solutionLock != null && solutionLock.LockEndTime < DateTime.Now)
+            {
+                return false;
+            }
+
+            // 没有锁，或者锁已经过期，允许锁定。
+            if (solutionLock == null)
+            {
+                solutionLock = new SolutionLock
+                {
+                    SolutionId = solutionId,
+                    LockClientId = Context.ConnectionId,
+                    LockEndTime = DateTime.Now.AddSeconds(LockTime)
+                };
+                db.SolutionLocks.Add(solutionLock);
+            }
+            else if (solutionLock.LockEndTime >= DateTime.Now)
+            {
+                solutionLock.LockClientId = Context.ConnectionId;
+                solutionLock.LockEndTime = DateTime.Now.AddSeconds(LockTime);
+            }
+
+            await db.SaveChangesAsync();
+            return true;
         }
 
         public override Task OnConnected()
@@ -47,5 +88,7 @@ namespace SdojWeb.SignalR
         {
             get { return DependencyResolver.Current.GetService<ApplicationDbContext>(); }
         }
+
+        public const int LockTime = 15;
     }
 }
