119d6e98b9dccdb56a6c1943142e4f205008012b
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-20 09:48:48 +0800

    加入SdojClient。

--- SdojJudger/Program.cs
+++ SdojJudger/Program.cs
@@ -1,64 +1,11 @@
-﻿using System;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR.Client;
-
-namespace SdojJudger
+﻿namespace SdojJudger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var connection = new HubConnection(AppSettings.ServerUrl);
-
-            var authCookie = AuthenticateUser(AppSettings.UserName, AppSettings.Password).Result;
-
-            if (authCookie != null)
-            {
-                connection.CookieContainer = new CookieContainer();
-                connection.CookieContainer.Add(authCookie);
-                Console.WriteLine("Welcome " + AppSettings.UserName);
-
-                var hub = connection.CreateHubProxy(AppSettings.HubName);
-                hub.On("DoWork", str => Console.WriteLine(str));
-
-                connection.Start().Wait();
-
-                while (true)
-                {
-                    var line = Console.ReadLine();
-                    hub.Invoke("Send", line);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Login failed");
-            }
-        }
-
-        private static async Task<Cookie> AuthenticateUser(string user, string password)
-        {
-            var request = WebRequest.CreateHttp(AppSettings.LoginUrl);
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.CookieContainer = new CookieContainer();
-
-            var authCredentials = string.Format("username={0}&password={1}",
-                WebUtility.UrlEncode(user),
-                WebUtility.UrlEncode(password));
-            var bytes = Encoding.UTF8.GetBytes(authCredentials);
-            request.ContentLength = bytes.Length;
-
-            using (var requestStream = await request.GetRequestStreamAsync())
-            {
-                requestStream.Write(bytes, 0, bytes.Length);
-            }
-
-            using (var response = await request.GetResponseAsync() as HttpWebResponse)
-            {
-                return response.Cookies[AppSettings.CookieName];
-            }
+            var client = new SdojClient();
+            client.Run().Wait();
         }
     }
 }
---
+++ SdojJudger/SdojClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SdojJudger
+{
+    public class SdojClient
+    {
+        public async Task Run()
+        {
+            var authCookie = await AuthenticateUser(AppSettings.UserName, AppSettings.Password);
+
+
+            if (authCookie == null)
+            {
+                Console.WriteLine("error: login failed for user {0}", AppSettings.UserName);
+            }
+            else
+            {
+                var connection = new HubConnection(AppSettings.ServerUrl) {CookieContainer = new CookieContainer()};
+                connection.CookieContainer.Add(authCookie);
+                var hub = connection.CreateHubProxy(AppSettings.HubName);
+                hub.On("DoWork", str => Console.WriteLine(str));
+                await connection.Start();
+
+                while (true)
+                {
+                    var line = Console.ReadLine();
+                    await hub.Invoke("Send", line);
+                }
+            }
+        }
+
+        private async Task<Cookie> AuthenticateUser(string user, string password)
+        {
+            var request = WebRequest.CreateHttp(AppSettings.LoginUrl);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+
+            var authCredentials = string.Format("username={0}&password={1}",
+                WebUtility.UrlEncode(user),
+                WebUtility.UrlEncode(password));
+            var bytes = Encoding.UTF8.GetBytes(authCredentials);
+            request.ContentLength = bytes.Length;
+
+            using (var requestStream = await request.GetRequestStreamAsync())
+            {
+                requestStream.Write(bytes, 0, bytes.Length);
+            }
+
+            using (var response = await request.GetResponseAsync() as HttpWebResponse)
+            {
+// ReSharper disable once PossibleNullReferenceException
+                return response.Cookies[AppSettings.CookieName];
+            }
+        }
+    }
+}
