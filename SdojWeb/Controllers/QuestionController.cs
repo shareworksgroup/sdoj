4f3f7cc3c05c6de79aed51bc2b507441bfa28a00
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-22 10:47:45 +0800

    删除QuestionController多余的

--- SdojWeb/Controllers/QuestionController.cs
+++ SdojWeb/Controllers/QuestionController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using AutoMapper;
 using SdojWeb.Infrastructure.Extensions;
 using SdojWeb.Infrastructure.Identity;
 using SdojWeb.Manager;
