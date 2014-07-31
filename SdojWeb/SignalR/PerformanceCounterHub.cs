940e969262ece0ba385ec479b083339da96f9086
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-31 20:25:54 +0800

    1分钟自动Dispose pcHub。

--- SdojWeb/SignalR/PerformanceCounterHub.cs
+++ SdojWeb/SignalR/PerformanceCounterHub.cs
@@ -14,20 +14,35 @@ namespace SdojWeb.SignalR
 
         public override Task OnConnected()
         {
+            _disposeTime = DateTime.Now.AddSeconds(1.0);
+
             if (Interlocked.CompareExchange(ref Started, 1, 0) == 0)
             {
                 var ps = Process.GetCurrentProcess();
                 var timer = new System.Timers.Timer(1000.0) {Enabled = true, AutoReset = true};
 
-                timer.Elapsed += (o, e) => 
-                    Clients.All.pc(
-                        ps.TotalProcessorTime.ToString(@"hh\:mm\:ss"),
-                        (DateTime.Now - ps.StartTime).ToString(@"hh\:mm\:ss"),
-                        ps.Threads.Count,
-                        GC.GetTotalMemory(false));
+                timer.Elapsed += (o, e) =>
+                {
+                    if (DateTime.Now > _disposeTime)
+                    {
+                        timer.Dispose();
+                        ps.Dispose();
+                        Interlocked.CompareExchange(ref Started, 0, 1);
+                    }
+                    else
+                    {
+                        Clients.All.pc(
+                            ps.TotalProcessorTime.ToString(@"hh\:mm\:ss"),
+                            (DateTime.Now - ps.StartTime).ToString(@"hh\:mm\:ss"),
+                            ps.Threads.Count,
+                            GC.GetTotalMemory(false));
+                    }
+                };  
             }
 
             return base.OnConnected();
         }
+
+        private DateTime _disposeTime;
     }
 }
