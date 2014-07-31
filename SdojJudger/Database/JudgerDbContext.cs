32affa184f54e878025e4a761203b3fb15af487b
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-07-31 22:44:22 +0800

    给客户端加入DbContext。

---
+++ SdojJudger/Database/JudgerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SdojJudger.Database
+{
+    public class JudgerDbContext : DbContext
+    {
+        public JudgerDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<SolutionEntity> SolutionEntities { get; set; }
+
+        public static JudgerDbContext Create()
+        {
+            var db = new JudgerDbContext();
+#if DEBUG
+            db.Database.Log += Console.WriteLine;
+#endif
+            return db;
+        }
+    }
+}
---
+++ SdojJudger/Database/SolutionEntity.cs
@@ -0,0 +1,13 @@
+﻿using SdojJudger.Models;
+
+namespace SdojJudger.Database
+{
+    public class SolutionEntity
+    {
+        public int Id { get; set; }
+
+        public Languages Language { get; set; }
+
+        public float FullMemoryLimitMb { get; set; }
+    }
+}
