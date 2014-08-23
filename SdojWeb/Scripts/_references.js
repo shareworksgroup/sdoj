03cd69b5c0b6624dee5fbc2633cfdc4144120023
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-08-23 10:48:04 +0800

    更新modernizr。

--- SdojWeb/Scripts/_references.js
+++ SdojWeb/Scripts/_references.js
@@ -7,6 +7,6 @@
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-3.1.0.js" />
-/// <reference path="modernizr-2.7.2.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <reference path="respond.js" />
 /// <reference path="respond.matchmedia.addlistener.js" />
