27aa47a3137dd9f067e0fe1c4fbdad0acddce6f8
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-09-20 15:15:28 +0800

    调整解答页面。

--- SdojWeb/Infrastructure/Extensions/LinkExtensions.cs
+++ SdojWeb/Infrastructure/Extensions/LinkExtensions.cs
@@ -27,5 +27,38 @@ namespace SdojWeb.Infrastructure.Extensions
             }
             return html.ActionLink(displayName, actionName, new { orderBy = memberName, asc });
         }
+
+        public static MvcHtmlString EnumDropDownList<TEnum>(this HtmlHelper html, string name, TEnum? value, string optionalLabel, object htmlAttribute) where TEnum:struct
+        {
+            var optionalItem = new SelectListItem {Value = "", Text = optionalLabel};
+            var selectList = new List<SelectListItem> {optionalItem};
+            selectList.AddRange(EnumHelper.GetSelectList(typeof (TEnum)));
+
+            if (value == null)
+            {
+                selectList[0].Selected = true;
+                return html.DropDownList(name, selectList, htmlAttribute);
+            }
+
+            bool finded = false;
+
+            for (var i = 1; i < selectList.Count; ++i)
+            {
+                var item = selectList[i];
+                if (item.Value == value.ToString())
+                {
+                    item.Selected = true;
+                    finded = true;
+                }
+            }
+
+            if (!finded)
+            {
+                selectList[0].Selected = true;
+                return html.DropDownList(name, selectList, htmlAttribute);
+            }
+
+            return html.DropDownList(name, selectList, htmlAttribute);
+        }
     }
 }
---
+++ SdojWeb/Infrastructure/Extensions/RouteValueDictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Routing;
+
+namespace SdojWeb.Infrastructure.Extensions
+{
+    public static class RouteValueDictionaryExtensions
+    {
+        public static RouteValueDictionary CopySetPaged(this RouteValueDictionary route, int page, string orderBy, bool? asc)
+        {
+            var r = new RouteValueDictionary(route);
+            r["page"] = page;
+            r["orderBy"] = orderBy;
+            r["asc"] = asc;
+            return r;
+        }
+    }
+}
