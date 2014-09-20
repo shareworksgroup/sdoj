aef13381bad5c1fceeb033d63fb2a52e11b70c3b
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-09-21 00:53:53 +0800

    ValidateInput(false)

--- SdojWeb/Controllers/SolutionController.cs
+++ SdojWeb/Controllers/SolutionController.cs
@@ -127,7 +127,7 @@ namespace SdojWeb.Controllers
         // POST: Solution/Create/id
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost, ValidateAntiForgeryToken]
+        [HttpPost, ValidateAntiForgeryToken, ValidateInput(false)]
         public async Task<ActionResult> Create(SolutionCreateModel model)
         {
             if (ModelState.IsValid)
