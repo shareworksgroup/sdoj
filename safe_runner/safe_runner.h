0a141b0e125ab6a310dc44dabc11b0a8c102417c
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-10-23 11:29:31 +0800

    删除无用代码。

--- safe_runner/safe_runner.h
+++ safe_runner/safe_runner.h
@@ -6,35 +6,3 @@
 bool judge(api_judge_info const & aji, api_judge_result & ajr);
 
 void free_judge_result(api_judge_result & result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void cluck();
-int get3();
-int string_length(wchar_t *);
-
-struct string_table
-{
-	wchar_t * str1;
-	int length1;
-	wchar_t * str2;
-	int length2;
-	wchar_t * str3;
-	int length3;
-};
-bool concat_string_table(string_table const & table, wchar_t * result, int length);
-bool concat_string_args(
-	wchar_t const * str1, int length1, wchar_t const * str2, int length2, wchar_t const * str3, int length3, wchar_t * result, int length);
