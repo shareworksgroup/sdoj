4da1cbd2d889a757ab832f1a23c2e29b7de8707d
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-11-03 17:19:50 +0800

    Run_can_accept_unicode_input

--- Sandbox.Tests/RunTest/BasicRunTest.cs
+++ Sandbox.Tests/RunTest/BasicRunTest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 using FluentAssertions;
+using SdojJudger.Compiler;
+using SdojJudger.Compiler.Infrastructure;
 using SdojJudger.SandboxDll;
 using Xunit;
+using Xunit.Sdk;
 
 namespace SandboxTests.RunTest
 {
@@ -47,7 +51,7 @@ namespace SandboxTests.RunTest
 
             using (var reader = new StreamReader(ior.OutputReadStream))
             {
-                var content = reader.ReadToEnd();
+                var content = reader.ReadToEndAsync().Result;
                 var actual = content.Split(new[] {'\\'});
                 var actualDomain = actual[0];
                 var actualUsername = actual[1].Trim();
@@ -60,5 +64,57 @@ namespace SandboxTests.RunTest
             }
             Process.GetProcessesByName("whoami").Length.Should().Be(0);
         }
+
+
+
+        [Fact]
+        [Trait("Run", "Basic")]
+        public void Run_can_accept_unicode_input()
+        {
+            // Arrange
+            var compiler = new CSharpCompiler();
+            var asm = compiler.Compile(Code);
+            var path = asm.PathToAssembly;
+
+            var info = new SandboxRunInfo
+            {
+                LimitProcessCount = false,
+                MemoryLimitMb = 10.0f,
+                Path = path,
+                TimeLimitMs = 1000
+            };
+
+            // Act
+            var ior = Sandbox.BeginRun(info);
+
+            // Assert
+            using (var writer = new StreamWriter(ior.InputWriteStream))
+            {
+                writer.Write("Flash中文");
+            }
+            var reader = new StreamReader(ior.OutputReadStream);
+            var readTask = reader.ReadToEndAsync();
+
+            var res = Sandbox.EndRun(ior.InstanceHandle);
+            readTask.Result.Should().Be("Hey Flash中文!");
+
+            res.Succeed.Should().BeTrue();
+            res.MemoryMb.Should().BeGreaterThan(0);
+            res.TimeMs.Should().BeGreaterOrEqualTo(0);
+            compiler.Dispose();
+        }
+
+        public const string Code =
+            "using System;                             " +
+            "using System.Text;                        " +
+            "                                          " +
+            "class Program                             " +
+            "{                                         " +
+            "    static void Main(string[] args)       " +
+            "    {                                     " +
+            "        var str = Console.In.ReadToEnd(); " +
+            "        Console.Write(\"Hey {0}!\", str); " +
+            "    }                                     " +
+            "}                                         ";
     }
 }
