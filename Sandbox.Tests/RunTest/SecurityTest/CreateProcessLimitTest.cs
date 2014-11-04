7bbe5277c27b7d05f00a37f346cdd443573a015e
flysha.zhou <flysha.zhou@starlight-sms.com>
2014-11-04 12:02:57 +0800

    CreateProcessLimitTest

---
+++ Sandbox.Tests/RunTest/SecurityTest/CreateProcessLimitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace SandboxTests.RunTest.SecurityTest
+{
+    public class CreateProcessLimitTest
+    {
+        [Fact]
+        public void Create_multiple_process_in_limit_should_be_ok()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Create_more_process_than_limit_should_fail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
