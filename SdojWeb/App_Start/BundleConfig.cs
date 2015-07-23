79812df14eb1e4220d3538aa0e39a83211237e2b
flysha <flysha@live.com>
2015-07-23 16:31:04 +0800

    drop modernizer

--- SdojWeb/App_Start/BundleConfig.cs
+++ SdojWeb/App_Start/BundleConfig.cs
@@ -44,11 +44,6 @@ namespace SdojWeb
 
             bundles.Add(new ScriptBundle("~/bundles/signalr").Include(
                 "~/Scripts/jquery.signalR-{version}.js"));
-
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
         }
     }
 
