a1d7bac3cce051316409fab710dbfc78818ee52e
flysha <flysha@live.com>
2015-07-29 09:38:13 +0800

    + comments @ emailservices

--- SdojWeb/Models/Identity/EmailService.cs
+++ SdojWeb/Models/Identity/EmailService.cs
@@ -9,23 +9,33 @@ namespace SdojWeb.Models
     {
         public async Task SendAsync(IdentityMessage message)
         {
+            // no-reply@sdcb.in
             var from = AppSettings.EmailFrom;
-            var password = AppSettings.EmailPassword;
-            var host = AppSettings.SmtpHost;
-            var port = AppSettings.SmtpPort;
 
+            // 123456
+            var password = AppSettings.EmailPassword;
+
+            // stmp.ym.163.com
+            var host = AppSettings.SmtpHost;
+
+            // 25
+            var port = AppSettings.SmtpPort;
+            
+            // Destination为收件箱，如397482054@qq.com
             var mail = new MailMessage(from, message.Destination)
             {
-                Subject = message.Subject,
-                Body = message.Body,
-                IsBodyHtml = true,
+                Subject = message.Subject, // 标题
+                Body = message.Body, // 内容
+                IsBodyHtml = true, // 内容是否为HTML格式
             };
 
             using (var client = new SmtpClient(host, port))
             {
+                // from是你的邮箱地址；password是密码
                 client.Credentials = new NetworkCredential(from, password);
                 client.EnableSsl = true;
 
+                // 发送
                 await client.SendMailAsync(mail);
             }
         }
